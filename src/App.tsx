9a973e292477ed27d836cce424d2aac618c6f1cb
chenz2021 <zhouchen9288@gmail.com>
2022-02-17 18:24:56 -0500

    Revolving light with grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from "./App.module.css";
 import { Header, Footer } from "./components";
+import { Row, Col } from "antd";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <div className={styles['page-content']}>
+
+      </div>
       <Footer />
     </div>
   );
