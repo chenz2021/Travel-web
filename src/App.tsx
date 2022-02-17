5bb21ac06b30bdc954ad6c442fc3c248115fe96e
chenz2021 <zhouchen9288@gmail.com>
2022-02-17 16:08:21 -0500

    Header Title

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
         {/* top-header */}
         <div className={styles["top-header"]}>
           <div className={styles.inner}>
-            <Typography.Text>让旅游更幸福</Typography.Text>
+            <Typography.Text>Lighten Up Your Trip</Typography.Text>
             <Dropdown.Button
               style={{ marginLeft: 15 }}
               overlay={
@@ -23,46 +23,46 @@ const App: React.FC = () => {
               }
               icon={<GlobalOutlined />}
             >
-              语言
+              Language
             </Dropdown.Button>
             <Button.Group className={styles["button-group"]}>
-              <Button>注册</Button>
-              <Button>登陆</Button>
+              <Button>Sign Up</Button>
+              <Button>Sign In</Button>
             </Button.Group>
           </div>
         </div>
         <Layout.Header className={styles["main-header"]}>
           <img src={logo} alt="logo" className={styles["App-logo"]} />
           <Typography.Title level={3} className={styles.title}>
-            React旅游网
+            React Travel
           </Typography.Title>
           <Input.Search
-            placeholder={"请输入旅游目的地、主题、或关键字"}
+            placeholder={"Please Enter Keyword"}
             className={styles["search-input"]}
           />
         </Layout.Header>
         <Menu mode={"horizontal"} className={styles["main-menu"]}>
-          <Menu.Item key={1}>旅游首页</Menu.Item>
-          <Menu.Item key={2}>周末游</Menu.Item>
-          <Menu.Item key={3}>跟团游</Menu.Item>
-          <Menu.Item key="4"> 自由行 </Menu.Item>
-          <Menu.Item key="5"> 私家团 </Menu.Item>
-          <Menu.Item key="6"> 邮轮 </Menu.Item>
-          <Menu.Item key="7"> 酒店+景点 </Menu.Item>
-          <Menu.Item key="8"> 当地玩乐 </Menu.Item>
-          <Menu.Item key="9"> 主题游 </Menu.Item>
-          <Menu.Item key="10"> 定制游 </Menu.Item>
-          <Menu.Item key="11"> 游学 </Menu.Item>
-          <Menu.Item key="12"> 签证 </Menu.Item>
-          <Menu.Item key="13"> 企业游 </Menu.Item>
-          <Menu.Item key="14"> 高端游 </Menu.Item>
-          <Menu.Item key="15"> 爱玩户外 </Menu.Item>
-          <Menu.Item key="16"> 保险 </Menu.Item>
+          <Menu.Item key={1}>Home</Menu.Item>
+          <Menu.Item key={2}>Weekend</Menu.Item>
+          <Menu.Item key={3}>Group</Menu.Item>
+          <Menu.Item key="4"> Solo </Menu.Item>
+          <Menu.Item key="5"> Private </Menu.Item>
+          <Menu.Item key="6"> Cruise </Menu.Item>
+          <Menu.Item key="7"> Hotel+Attractions </Menu.Item>
+          <Menu.Item key="8"> Local </Menu.Item>
+          <Menu.Item key="9"> Theme </Menu.Item>
+          <Menu.Item key="10"> Custom </Menu.Item>
+          <Menu.Item key="11"> Study Tour </Menu.Item>
+          <Menu.Item key="12"> Visa </Menu.Item>
+          <Menu.Item key="13"> Business </Menu.Item>
+          <Menu.Item key="14"> VIP </Menu.Item>
+          <Menu.Item key="15"> Outdoor </Menu.Item>
+          <Menu.Item key="16"> Insurance </Menu.Item>
         </Menu>
       </div>
       <Layout.Footer>
         <Typography.Title level={3} style={{textAlign: 'center'}}>
-          版权所有 @ React 旅游网
+           @ React Travel
         </Typography.Title>
       </Layout.Footer>
     </div>
