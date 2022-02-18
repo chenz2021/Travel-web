7389c6e5b4abb6ed2d3d342ef53fcfc67051bd8c
chenz2021 <zhouchen9288@gmail.com>
2022-02-17 21:31:44 -0500

    Edit text in side menu

--- src/components/sideMenu/mockup.ts
+++ src/components/sideMenu/mockup.ts
@@ -1,77 +1,77 @@
 export const sideMenuList = [
     {
-      title: "主题旅游",
+      title: "Theme",
       subMenu: [
         {
-          title: "爸妈游",
+          title: "Parents",
           subMenu: ["祈福拜佛", "古镇游玩", "桂林山水", "红色之旅"],
         },
         {
-          title: "亲子游",
+          title: "Family",
           subMenu: ["上海迪士尼", "探险体验", "海洋公园", "感受名校"],
         },
         {
-          title: "蜜月游",
+          title: "Honeymoon",
           subMenu: ["浪漫海岛", "豪华酒店", "购物血拼", "全球婚礼"],
         },
       ],
     },
     {
-      title: "周边游",
+      title: "Local",
       subMenu: [
         {
-          title: "周边目的地",
+          title: "Local Attractions",
           subMenu: ["杭州", "南京", "苏州", "黄山"],
         },
         {
-          title: "热门景点",
+          title: "Most Popular",
           subMenu: ["普陀山", "千岛湖", "乌镇古镇", "宏村"],
         },
       ],
     },
     {
-      title: "国内游",
+      title: "Domestic",
       subMenu: [
         {
-          title: "广东",
+          title: "Guang Dong",
           subMenu: ["广州", "深圳", "珠海", "惠州"],
         },
         {
-          title: "浙江",
+          title: "Zhe Jiang",
           subMenu: ["杭州", "普陀山", "千岛湖", "宁波"],
         },
         {
-          title: "江苏",
+          title: "Jiang Su",
           subMenu: ["舟山", "乌镇", "干山", "湖州"],
         },
       ],
     },
     {
-      title: "东南亚",
+      title: "South East Asia",
       subMenu: [
         {
-          title: "热门目的地",
+          title: "Popular",
           subMenu: ["新加坡", "巴厘岛", "越南", "柬埔寨"],
         },
         {
-          title: "热门景点",
+          title: "Attractions",
           subMenu: ["下龙湾", "吴哥窟", "西哈努克", "槟城珍珠岛"],
         },
       ],
     },
     {
-      title: "欧洲",
+      title: "Europe",
       subMenu: [
         {
-          title: "热门邮轮航线",
+          title: "Popular Cruise",
           subMenu: ["欧洲河轮", "北欧邮轮", "爱琴海邮轮", "英伦三岛"],
         },
         {
-          title: "热门景点",
+          title: "Popular Attractions",
           subMenu: ["贝加尔湖", "普罗旺斯", "阿尔卑斯", "圣托里尼岛"],
         },
         {
-          title: "蜜月游",
+          title: "HoneyMoon",
           subMenu: ["极光中心", "俄罗斯远东地区", "英格兰少女峰", "玻璃海滩"],
         },
       ],
