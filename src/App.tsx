40c996df2ba54780b80457fc68670f7f13a1fbbf
chenz2021 <zhouchen9288@gmail.com>
2022-02-18 11:27:26 -0500

    404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />}/>
+          <Route path="/signIn" element={<h1>Sign In</h1>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>  
       </Router>     
     </div>
