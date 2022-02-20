894b7a1acb6bb68354355d58893382f3eed31d9d
chenz2021 <zhouchen9288@gmail.com>
2022-02-20 13:36:11 -0500

    Replace mockup data with real data on homepage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,7 +7,6 @@ import {
   ProductCollection,
 } from "../../components";
 import { Row, Col, Typography, Spin } from "antd";
-import { productList1, productList2, productList3 } from "./mockups";
 import sideImage from "../../assets/images/sider_2019_12-09.png";
 import sideImage2 from "../../assets/images/sider_2019_02-04.png";
 import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
