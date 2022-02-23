c242e4ca642d6fc7ea598b6359e01743d4ac5eab
chenz2021 <zhouchen9288@gmail.com>
2022-02-23 10:52:16 -0500

    Add pruduct detail page

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,52 +1,90 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Spin, Row, Col } from "antd"
+import { Spin, Row, Col } from "antd";
 import styles from "./DetailPage.module.css";
 import { Header, Footer, ProductIntro } from "../../components";
 import { DatePicker, Space } from "antd";
 
+const { RangePicker } = DatePicker;
+
 interface MatchParams {
-    touristRouteId: string,
+  touristRouteId: string;
 }
 
 export const DetailPage: React.FC = (): JSX.Element => {
-    const {touristRouteId} = useParams();
-    const [loading, setLoading] = useState<boolean>(true)
-    const [product, setProduct] = useState<any>(null)
-    const [error, setError] = useState<string | null>(null)
+  const { touristRouteId } = useParams();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [product, setProduct] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect (() => {
-    	const fetchData = async () => { 
-            setLoading(true);
-            try {
-                const { data } = await axios.get(`http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`);
-                setLoading(false);
-                setProduct(data);
-            } catch (error:any) {
-                setLoading(false)
-                setError(error.message)
-            }
-            }
-            fetchData();      
-        }, []);
-
-    if (loading) {
-        return (
-            <Spin
-            size="large"
-            style={{
-                marginTop: 200,
-                marginBottom: 200,
-                marginLeft: "auto",
-                marginRight: "auto",
-                width: "100%",
-            }}
-            />
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data } = await axios.get(
+          `http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`
         );
-        }
-        if (error) {
-            return <div>Error Occured: {error}</div>;
-        }    
-    return <h1>route ID: {touristRouteId}</h1>
-}
+        setProduct(data);
+        setLoading(false);
+      } catch (error:any) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  if (loading) {
+    return (
+      <Spin
+        size="large"
+        style={{
+          marginTop: 200,
+          marginBottom: 200,
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "100%",
+        }}
+      />
+    );
+  }
+  if (error) {
+    return <div>Error Ocurred: {error}</div>;
+  }
+  return (
+    <>
+      <Header />
+      <div className={styles["page-content"]}>
+        <div className={styles["product-intro-container"]}>
+          <Row>
+            <Col span={13}>
+              <ProductIntro
+                title={product.title}
+                shortDescription={product.description}
+                price={product.originalPrice}
+                coupons={product.coupons}
+                points={product.points}
+                discount={product.price}
+                rating={product.rating}
+                pictures={product.touristRoutePictures.map((p) => p.url)}
+              />
+            </Col>
+            <Col span={11}>
+              <RangePicker open style={{ marginTop: 20 }} />
+            </Col>
+          </Row>
+        </div>
+        <div className={styles["product-detail-anchor"]}></div>
+
+        <div id="feature" className={styles["product-detail-container"]}></div>
+
+        <div id="fees" className={styles["product-detail-container"]}></div>
+
+        <div id="notes" className={styles["product-detail-container"]}></div>
+
+        <div id="comments" className={styles["product-detail-container"]}></div>
+      </div>
+      <Footer />
+    </>
+  );
+};
