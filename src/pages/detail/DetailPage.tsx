45b7723b9c430f0818f2282d5c3768c7c34610e1
chenz2021 <zhouchen9288@gmail.com>
2022-02-23 12:02:12 -0500

    Comment mockup data

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -5,6 +5,8 @@ import { Spin, Row, Col } from "antd";
 import styles from "./DetailPage.module.css";
 import { Header, Footer, ProductIntro } from "../../components";
 import { DatePicker, Space, Divider, Typography, Anchor, Menu } from "antd";
+import { ProductComments } from "../../components";
+import { commentMockData as mockup } from "./mockup";
 
 const { RangePicker } = DatePicker;
 
@@ -71,29 +73,52 @@ export const DetailPage: React.FC = (): JSX.Element => {
           </Row>
         </div>
         <div className={styles["product-detail-anchor"]}></div>
-
-        <div id="feature" className={styles["product-detail-container"]}></div>
-            <Divider orientation={'center'}>
+            <Anchor className={styles["product-detail-anchor"]}>
+            <Menu mode="horizontal">
+                <Menu.Item key="1">
+                <Anchor.Link href="#feature" title="Feature"></Anchor.Link>
+                </Menu.Item>
+                <Menu.Item key="3">
+                <Anchor.Link href="#fees" title="Fees"></Anchor.Link>
+                </Menu.Item>
+                <Menu.Item key="4">
+                <Anchor.Link href="#notes" title="Notes"></Anchor.Link>
+                </Menu.Item>
+                <Menu.Item key="5">
+                <Anchor.Link href="#comments" title="Comments"></Anchor.Link>
+                </Menu.Item>
+            </Menu>
+            </Anchor>
+        <div id="feature" className={styles["product-detail-container"]}>
+        <Divider orientation={'center'}>
                 <Typography.Title level={3}>Feature</Typography.Title>
             </Divider>
             <div dangerouslySetInnerHTML={{__html: product.features}}
             style={{ margin: 50 }}>
             </div>
-        <div id="fees" className={styles["product-detail-container"]}></div>
+        </div>
+            
+        <div id="fees" className={styles["product-detail-container"]}>
         <Divider orientation={'center'}>
                 <Typography.Title level={3}>Fees</Typography.Title>
             </Divider>
             <div dangerouslySetInnerHTML={{__html: product.fees}}
             style={{ margin: 50 }}>
             </div>
-        <div id="notes" className={styles["product-detail-container"]}></div>
-            <Divider orientation={'center'}>
+        </div>
+        
+        <div id="notes" className={styles["product-detail-container"]}>
+        <Divider orientation={'center'}>
                 <Typography.Title level={3}>Notes</Typography.Title>
             </Divider>
             <div dangerouslySetInnerHTML={{__html: product.notes}}
             style={{ margin: 50 }}>
             </div>
-        <div id="comments" className={styles["product-detail-container"]}></div>
+        </div>
+            
+        <div id="comments" className={styles["product-detail-container"]}>
+            <ProductComments data={mockup}/>
+        </div>
       </div>
       <Footer />
     </>
