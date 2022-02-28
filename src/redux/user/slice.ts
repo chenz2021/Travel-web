5425cc066056d456156e7af2e1d84b913b641836
chenz2021 <zhouchen9288@gmail.com>
2022-02-28 15:45:19 -0500

    Implement sign in

--- src/redux/user/slice.ts
+++ src/redux/user/slice.ts
@@ -1,51 +1,52 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { sign } from "crypto";
 
 interface UserState {
-    loading: boolean;
-    error: string | null;
-    token: string | null;
+  loading: boolean;
+  error: string | null;
+  token: string | null;
 }
-const initialState: UserState = {
-    loading: true,
-    error: null,
-    token: null,
-  };
 
-export const SignIn = createAsyncThunk(
-    "user/signIn",
-    async (parameters: {
-        email: string,
-        password: string
-    }, thunkAPI) => {
-        const { data } = await axios.post(
-          `http://123.56.149.216:8080/auth/login`, {
-              email: parameters.email,
-              password: parameters.password
-          }
-        );
-        return data.token; 
-    }
-)
+const initialState: UserState = {
+  loading: false,
+  error: null,
+  token: null,
+};
+
+export const signIn = createAsyncThunk(
+  "user/signIn",
+  async (paramaters: {
+      email: string,
+      password: string,
+  }, thunkAPI) => {
+    const { data } = await axios.post(
+      `http://123.56.149.216:8080/auth/login`,{
+          email: paramaters.email,
+          password: paramaters.password
+      }
+    );
+    return data.token;
+  }
+);
 
 export const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {},
-    extraReducers: {
-        [SignIn.pending.type]: (state) => {
-            // return {...state, loading: true};
-            state.loading = true
-        },
-        [SignIn.fulfilled.type]: (state, action) => {
-            state.token = action.payload;
-            state.loading = false;
-            state.error = null
-        },
-        [SignIn.rejected.type]: (state, action) => {
-            state.loading = false
-            state.error = action.payload
-        }
-    }
-})
+  name: "user",
+  initialState,
+  reducers: {
+    
+  },
+  extraReducers: {
+    [signIn.pending.type]: (state) => {
+      state.loading = true;
+    },
+    [signIn.fulfilled.type]: (state, action) => {
+      state.token = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    [signIn.rejected.type]: (state, action: PayloadAction<string | null>) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  }
+});
